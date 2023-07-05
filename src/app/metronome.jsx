af098ea8cc5786404be51dadd87ea2a236b03d6f
Keith <keithjayjones@gmail.com>
2023-07-05 16:01:32 -0400

    Creates a metroonome and records lengths of taps

---
+++ src/app/metronome.jsx
@@ -0,0 +1,23 @@
+export default function Metronome({
+  metronomeOn,
+  tempo
+  }) {
+    let msTempo;
+    if (tempo) {
+      msTempo = 60000/tempo;
+    } else {
+      msTempo = 0;
+    }
+    const createMetStyle = (metronomeOn) => ({
+      width: '100px',
+      height: '100px',
+      backgroundColor: '#000000',
+      '-webkit-animation': metronomeOn ? `flickerAnimation ${msTempo}ms infinite` : '',
+      '-moz-animation': metronomeOn ? `flickerAnimation ${msTempo}ms infinite` : '',
+      '-o-animation': metronomeOn ? `flickerAnimation ${msTempo}ms infinite` : '',
+      '-animation': metronomeOn ? `flickerAnimation ${msTempo}ms infinite` : '',
+    });
+    return (
+      <div style={createMetStyle(metronomeOn)}></div>
+    )
+  }
