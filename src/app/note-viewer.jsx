7cdf640f8d53618f02b1d316a312301c7b80ccc0
Keith <keithjayjones@gmail.com>
2023-07-06 16:56:44 -0400

    Hard code image base

--- src/app/note-viewer.jsx
+++ src/app/note-viewer.jsx
@@ -3,10 +3,11 @@ import Image from 'next/image'
 export default function NoteViewer({
   rhythmList
   }) {
+    const basePath = process.env.IMAGE_PATH === undefined ? '/rhythm-finder' : process.env.IMAGE_PATH;
     const images = rhythmList.map((rhythm, index) => (
       <Image
         key={`image-${index}`}
-        src={`${process.env.IMAGE_PATH}/notes/${rhythm.noteValue}.png`}
+        src={`${basePath}/notes/${rhythm.noteValue}.png`}
         alt='Text'
         width={50}
         height={100}
