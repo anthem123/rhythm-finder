dfd209eda06bf642f46cd7e12f84ff1d1c1307c9
Keith <keithjayjones@gmail.com>
2023-07-06 11:59:09 -0400

    Added basePath

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/rhythm-finder',
   images: {
     // limit of 25 deviceSizes values
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
