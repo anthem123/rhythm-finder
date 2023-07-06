b4786c806521d044af4d8e0ca7b670614da9783f
Keith <keithjayjones@gmail.com>
2023-07-06 16:13:39 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/rhythm-finder'
+  basePath: '/rhythm-finder',
+  output: 'export',
 }
 
 module.exports = nextConfig
