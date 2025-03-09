38810baad5ba3da6c21ff097d901e92735fed35c
Keith <keithjayjones@gmail.com>
2025-03-09 13:36:48 -0400

    Attempting to refactor the metronome service worker

--- public/metronome-worker.js
+++ public/metronome-worker.js
@@ -1,23 +1,34 @@
-let timerID = null;
-let interval = 100;
+let interval = null;
+let bpm = 100;
 
-self.onmessage = function (e) {
-  if (e.data == "start") {
-    timerID = setInterval(function () {
-      postMessage("tick");
-    }, interval);
-  } else if (e.data.interval) {
-    interval = e.data.interval;
-    if (timerID) {
-      clearInterval(timerID);
-      timerID = setInterval(function () {
-        postMessage("tick");
-      }, interval);
+self.addEventListener("message", (event) => {
+    if (event.data.command === "start") {
+        bpm = event.data.tempo;
+        startMetronome();
+    } else if (event.data.command === "stop") {
+        stopMetronome();
+    } else if (event.data.command === "update-bpm") {
+        bpm = event.data.tempo;
+        restartMetronome();
     }
-  } else if (e.data == "stop") {
-    clearInterval(timerID);
-    timerID = null;
-  }
-};
+});
 
-postMessage("hi there");
+function startMetronome() {
+    stopMetronome(); // Clear any previous interval
+    const intervalTime = (60 / bpm) * 1000;
+
+    interval = setInterval(() => {
+        self.clients.matchAll().then((clients) => {
+            clients.forEach((client) => client.postMessage({ type: "tick" }));
+        });
+    }, intervalTime);
+}
+
+function stopMetronome() {
+    if (interval) clearInterval(interval);
+}
+
+function restartMetronome() {
+    stopMetronome();
+    startMetronome();
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import MetronomeView from "../components/metronome-view/metronome-view";
 import MetronomeSelecter from "../components/metronome-selector/metronome-selecter";
 // import NoteViewer from "../components/note-viewer/note-viewer";
@@ -25,12 +25,55 @@ export default function Home() {
   const [displayMetModal, setDisplayMetModal] = useState(false);
   const [metronomeOn, setMetronomeOn] = useState(false);
   const [metronomeText, setMetronomeText] = useState(playIcon);
+  const [worker, setWorker] = useState(null);
+  const metronomeRef = useRef(null);
 
   useEffect(() => {
-    // call api or anything
-    const basePath = process.env.NEXT_PUBLIC_RESOURCE_PATH ?? "/rhythm-finder";
-    metronomeUtil.init(basePath);
+    if ("serviceWorker" in navigator && !worker) {
+      navigator.serviceWorker.register("rhythm-finder/metronome-worker.js").then((registration) => {
+        console.log("Service Worker registered:", registration);
+        if (registration.active) {
+          // @ts-ignore
+          setWorker(registration.active);
+          registration.addEventListener("message", (event) => {
+            if (event.data.type === "tick") {
+              playClick();
+            }
+          });
+        }
+        else {
+          navigator.serviceWorker.ready.then((sw) => {
+            // @ts-ignore
+            setWorker(sw.active);
+            sw.addEventListener("message", (event) => {
+              if (event.data.type === "tick") {
+                playClick();
+              }
+            });
+          });
+        }
+      });
+
+      // navigator.serviceWorker.addEventListener("message", (event) => {
+      //   if (event.data.type === "tick") {
+      //     playClick();
+      //   }
+      // });
+    }
   });
+
+  const playClick = () => {
+    // const starting = metronomeUtil.play(tempo);
+    // setStartTime(starting);
+    console.log("playClick");
+
+  }
+
+  // useEffect(() => {
+  //   // call api or anything
+  //   const basePath = process.env.NEXT_PUBLIC_RESOURCE_PATH ?? "/rhythm-finder";
+  //   metronomeUtil.init(basePath);
+  // });
 
   const addRhythm = () => {
     if (newRhythm) {
@@ -68,19 +111,24 @@ export default function Home() {
   };
 
   const toggleMetronome = () => {
+    if (!worker) return;
+
     if (metronomeOn) {
-      setMetronomeOn(false);
+      // @ts-ignore
+      worker.postMessage({ command: "stop" });
       setMetronomeText(playIcon);
       setNewRhythm(true);
 
-      metronomeUtil.stop();
+      // metronomeUtil.stop();
     } else {
-      setMetronomeOn(true);
+      // @ts-ignore
+      worker.postMessage({ command: "start", tempo });
       setMetronomeText(pauseIcon);
 
-      const starting = metronomeUtil.play(tempo);
-      setStartTime(starting);
+      // const starting = metronomeUtil.play(tempo);
+      // setStartTime(starting);
     }
+    setMetronomeOn(!metronomeOn);
   };
 
   const onMetModalSelect = () => {
