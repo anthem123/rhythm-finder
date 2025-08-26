0729f86174e0b7cd0a13106bef28a8b6ba9bf067
Keith <keithjayjones@gmail.com>
2025-08-26 19:42:39 -0400

    Maybe fix for deployment?

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: "/rhythm-finder",
   output: "export",
-  // images: { unoptimized: true }
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
